900743baad5991789358e4dc56fe2e74d5678014
Ieriz Nicolle <58625682+ieriznicolle@users.noreply.github.com>
2020-01-15 15:42:51 +0800

    Add files via upload

---
+++ Segments/Segments/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SevenSegment
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button1.FlatAppearance.BorderSize = 2
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(133, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 26)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button2.FlatAppearance.BorderSize = 2
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(253, 114)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(28, 93)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button3.FlatAppearance.BorderSize = 2
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(253, 245)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(28, 93)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button4.FlatAppearance.BorderSize = 2
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Location = New System.Drawing.Point(133, 344)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(122, 26)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button5.FlatAppearance.BorderSize = 2
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Location = New System.Drawing.Point(108, 245)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(28, 93)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button6.FlatAppearance.BorderSize = 2
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Location = New System.Drawing.Point(108, 114)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(28, 93)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button7.FlatAppearance.BorderSize = 2
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Location = New System.Drawing.Point(133, 213)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(122, 26)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.MistyRose
+        Me.Button8.FlatAppearance.BorderColor = System.Drawing.Color.MistyRose
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Font = New System.Drawing.Font("Adobe Gothic Std B", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(133, 401)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(122, 30)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "START"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(105, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 16)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Ieriz Nicolle B. Gonzalez"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(145, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 16)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "BSCPE 5 - 4"
+        '
+        'SevenSegment
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Ivory
+        Me.ClientSize = New System.Drawing.Size(388, 453)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "SevenSegment"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "7 Segments Display"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Segments/Segments/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class SevenSegment
+
+    Dim counter As Int16
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If Button8.Text = "START" Or Button8.Text = "RESTART" Then
+            Timer1.Interval = 1000
+            Timer1.Start()
+
+        ElseIf Button8.Text = "STOP" Then
+            Timer1.Stop()
+            counter = 0
+            Button8.Text = "RESTART"
+
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        counter += 1
+        Button8.Text = "STOP"
+
+        Button1.BackColor = Color.Crimson
+        Button2.BackColor = Color.Crimson
+        Button3.BackColor = Color.Crimson
+        Button4.BackColor = Color.Crimson
+        Button5.BackColor = Color.Crimson
+        Button6.BackColor = Color.Crimson
+        Button7.BackColor = Color.Ivory
+
+        If counter = 1 Then
+            Button1.BackColor = Color.Ivory
+            Button2.BackColor = Color.Crimson
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Ivory
+            Button5.BackColor = Color.Ivory
+            Button6.BackColor = Color.Ivory
+            Button7.BackColor = Color.Ivory
+
+        ElseIf counter = 2 Then
+            Button1.BackColor = Color.Crimson
+            Button2.BackColor = Color.Crimson
+            Button3.BackColor = Color.Ivory
+            Button4.BackColor = Color.Crimson
+            Button5.BackColor = Color.Crimson
+            Button6.BackColor = Color.Ivory
+            Button7.BackColor = Color.Crimson
+
+        ElseIf counter = 3 Then
+            Button1.BackColor = Color.Crimson
+            Button2.BackColor = Color.Crimson
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Crimson
+            Button5.BackColor = Color.Ivory
+            Button6.BackColor = Color.Ivory
+            Button7.BackColor = Color.Crimson
+
+        ElseIf counter = 4 Then
+            Button1.BackColor = Color.Ivory
+            Button2.BackColor = Color.Crimson
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Ivory
+            Button5.BackColor = Color.Ivory
+            Button6.BackColor = Color.Crimson
+            Button7.BackColor = Color.Crimson
+
+        ElseIf counter = 5 Then
+            Button1.BackColor = Color.Crimson
+            Button2.BackColor = Color.Ivory
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Crimson
+            Button5.BackColor = Color.Ivory
+            Button6.BackColor = Color.Crimson
+            Button7.BackColor = Color.Crimson
+
+        ElseIf counter = 6 Then
+            Button1.BackColor = Color.Crimson
+            Button2.BackColor = Color.Ivory
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Crimson
+            Button5.BackColor = Color.Crimson
+            Button6.BackColor = Color.Crimson
+            Button7.BackColor = Color.Crimson
+
+        ElseIf counter = 7 Then
+            Button1.BackColor = Color.Crimson
+            Button2.BackColor = Color.Crimson
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Ivory
+            Button5.BackColor = Color.Ivory
+            Button6.BackColor = Color.Ivory
+            Button7.BackColor = Color.Ivory
+
+        ElseIf counter = 8 Then
+            Button1.BackColor = Color.Crimson
+            Button2.BackColor = Color.Crimson
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Crimson
+            Button5.BackColor = Color.Crimson
+            Button6.BackColor = Color.Crimson
+            Button7.BackColor = Color.Crimson
+
+        ElseIf counter = 9 Then
+            Button1.BackColor = Color.Crimson
+            Button2.BackColor = Color.Crimson
+            Button3.BackColor = Color.Crimson
+            Button4.BackColor = Color.Crimson
+            Button5.BackColor = Color.Ivory
+            Button6.BackColor = Color.Crimson
+            Button7.BackColor = Color.Crimson
+        End If
+
+        If counter >= 10 Then
+            Timer1.Stop()
+            counter = 0
+            Button8.Text = "START"
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+    End Sub
+End Class
